ae43a0f875fa638e74b5d2fe87ce1b5a55d3e852
ajkkln <alinalukyanova6@gmail.com>
2024-12-23 19:19:41 +0400

    Update tsk1.py

--- tsk1.py
+++ tsk1.py
@@ -1,16 +1,15 @@
 from typing import Tuple
+
 def pull(n: int) -> Tuple[int, int]:
     min_4_star = 0
     min_5_star = 0
     
-   
+
     min_5_star = n // 90
+    min_4_star = (n // 10)
+    if n % 10 == 0:
+        min_4_star = min_4_star - 1
     
-    
-    min_4_star = n // 10
-    min_4_star = min_4_star - min_5_star 
     return min_4_star, min_5_star
-
-
-print(pull(24))  
+print(pull(99))
 
